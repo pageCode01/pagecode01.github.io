73819f1410080ba18305096758c4f3f1f8184c6d
AndyPythonCode <andyarciniegas24@gmail.com>
2021-05-11 22:07:58 -0400

    init

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://github.com/pageCode01/pagecode01.github.io.git master:gh-pages
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
